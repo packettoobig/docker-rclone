fae9aa893963d2e11344852f9c4821a68233c699
packettoobig <53047591+packettoobig@users.noreply.github.com>
2022-07-10 17:10:03 +0200

    Implement start and end job

--- sync.sh
+++ sync.sh
@@ -9,23 +9,27 @@ then
   echo "WARNING: A previous sync is still running. Skipping new sync command."
 else
 
-echo $$ > /tmp/sync.pid
+  echo $$ > /tmp/sync.pid
 
-if test "$(rclone ls --max-depth 2 $SYNC_SRC $RCLONE_OPTS)"; then
-  # the source directory is not empty
-  # it can be synced without clear data loss
-  echo "INFO: Starting rclone sync $SYNC_SRC $SYNC_DEST $RCLONE_OPTS $SYNC_OPTS"
-  rclone sync $SYNC_SRC $SYNC_DEST $RCLONE_OPTS $SYNC_OPTS
-
-  if [ -z "$CHECK_URL" ]
-  then
-    echo "INFO: Define CHECK_URL with https://healthchecks.io to monitor sync job"
+  if test "$(rclone ls --max-depth 2 $SYNC_SRC $RCLONE_OPTS)"; then
+    # the source directory is not empty
+    # it can be synced without clear data loss
+    if [ -z "$CHECK_URL" ]
+    then
+      echo "INFO: Define CHECK_URL with https://healthchecks.io to monitor sync job"
+      echo "INFO: Starting rclone sync $SYNC_SRC $SYNC_DEST $RCLONE_OPTS $SYNC_OPTS"
+      rclone sync $SYNC_SRC $SYNC_DEST $RCLONE_OPTS $SYNC_OPTS
+    else
+      # Inform https://healthchecks.io that the job started
+      wget $CHECK_URL/start -O /dev/null -O /dev/null
+      echo "INFO: Starting rclone sync $SYNC_SRC $SYNC_DEST $RCLONE_OPTS $SYNC_OPTS"
+      rclone sync $SYNC_SRC $SYNC_DEST $RCLONE_OPTS $SYNC_OPTS
+      # Inform https://healthchecks.io that the job is over
+      wget $CHECK_URL -O /dev/null
+    fi
   else
-    wget $CHECK_URL -O /dev/null
+    echo "WARNING: Source directory is empty. Skipping sync command."
   fi
-else
-  echo "WARNING: Source directory is empty. Skipping sync command."
-fi
 
 rm -f /tmp/sync.pid
 
